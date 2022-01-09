21dc4ac618918c0c21e0575c7d96f6bf843c11a2
lvictor215 <luizvpeixoto@gmail.com>
2022-01-09 16:12:05 -0300

    Criado validador e preenchimento da palavra

--- forca.py
+++ forca.py
@@ -4,6 +4,9 @@ from random import randint
 palavra = 'camisa'
 digitadas = list()
 tentativas = 3
+contador = 0
+var = ''
+pronta = list()
 
 # funções
 
@@ -26,18 +29,45 @@ def linha(msg='', tamanho=50, caractere='='):
 
 while tentativas > 0:
 
-    linha('Digite uma letra')
-    letra = input("Digite a letra desejada: ").strip().lower()[0]
-
+    while True:
+        linha('Digite uma letra')
+        letra = input("Digite a letra desejada: ").strip().lower()[0]
+        if letra not in digitadas:
+            break
+        else:
+            linha("Digite uma letra que não tenha sido digitada!")
     if letra in palavra:
+
+        for a, b, in enumerate(palavra):
+            if b == letra:
+                contador += 1
+                pronta.append(b)
+                pronta.append(a)
 
         linha(f"Encontrada a letra {letra}")
 
     else:
-
         linha(f"Não encontrada a letra {letra}")
-        tentativas -= 1
 
     digitadas.append(letra)
-    
+
+    for x in range(0, len(palavra)):
+        flag = 0
+        for y in range(0, len(pronta)):
+            if x == pronta[y]:
+                var += pronta[y - 1]
+                flag = 1
+
+        if flag == 0:
+            var += '_'
+    linha(var)
+    var = ''
+
+    if len(pronta) == len(palavra) * 2:
+        print("Você ganhou!")
+        break
+    elif tentativas == 0:
+        print("Você perdeu!")
+
 print(digitadas)
+print(pronta)
