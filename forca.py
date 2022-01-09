b5a7031c267cb5eaef3e0f4b04a483e17a37c99f
lvictor215 <luizvpeixoto@gmail.com>
2022-01-09 17:14:44 -0300

    Corrigida ordens de apresentação e organizando

--- forca.py
+++ forca.py
@@ -10,18 +10,6 @@ contador = 0
 var = ''
 pronta = list()
 
-# funções
-
-# def inputint(msg=''):
-#     while True:
-#         vlr = input(msg).strip()
-#         try:
-#             vlr = int(vlr)
-#             break
-#         except:
-#             print("Erro! Apenas números inteiros são permitidos, tente novamente!")
-#     return vlr
-
 
 def linha(msg='', tamanho=50, caractere='='):
     print(caractere * tamanho)
@@ -29,45 +17,8 @@ def linha(msg='', tamanho=50, caractere='='):
     print(caractere * tamanho)
 
 
-palavra = palavras[randint(0, len(palavras) - 1)]
-
-while tentativas > 0:
-
-    for x in range(0, len(palavra)):
-        flag = 0
-        for y in range(0, len(pronta)):
-            if x == pronta[y]:
-                var += pronta[y - 1]
-                flag = 1
-                var += ' '
-
-        if flag == 0:
-            var += '_ '
-    linha(var)
-    var = ''
-    while True:
-        linha('Digite uma letra')
-        letra = input("Digite a letra desejada: ").strip().lower()[0]
-        if letra not in digitadas:
-            break
-        else:
-            linha("Digite uma letra que não tenha sido digitada!")
-    if letra in palavra:
-
-        for a, b, in enumerate(palavra):
-            if b == letra:
-                contador += 1
-                pronta.append(b)
-                pronta.append(a)
-
-        linha(f"Encontrada a letra {letra}")
-
-    else:
-        linha(f"Não encontrada a letra {letra}")
-        tentativas -= 1
-
-    digitadas.append(letra)
-
+def validar():
+    global tentativas
     if tentativas == 6:
         print('=' * 50)
         print('o'.center(50))
@@ -98,8 +49,55 @@ while tentativas > 0:
         print('o'.center(50))
         print(''.center(50))
         print(''.center(50))
-    elif tentativas == 0:
+
+
+palavra = palavras[randint(0, len(palavras) - 1)]
+
+while tentativas > 0:
+
+    for x in range(0, len(palavra)):
+        flag = 0
+        for y in range(0, len(pronta)):
+            if x == pronta[y]:
+                var += pronta[y - 1]
+                flag = 1
+                var += ' '
+
+        if flag == 0:
+            var += '_ '
+    linha(var)
+    var = ''
+
+    validar()
+
+    while True:
+        linha('Digite uma letra')
+        letra = input("Digite a letra desejada: ").strip().lower()[0]
+        if letra not in digitadas:
+            break
+        else:
+            linha("Digite uma letra que não tenha sido digitada!")
+    if letra in palavra:
+
+        for a, b, in enumerate(palavra):
+            if b == letra:
+                contador += 1
+                pronta.append(b)
+                pronta.append(a)
+
+        linha(f"Encontrada a letra {letra}")
+
+    else:
+        linha(f"Não encontrada a letra {letra}")
+        tentativas -= 1
+
+    digitadas.append(letra)
+
+    if tentativas == 0:
         print("Você perdeu!")
-    if len(pronta) == len(palavra) * 2:
+        print("A palavra correta era:")
+        linha(palavra)
+    elif len(pronta) == len(palavra) * 2:
         print("Você ganhou!")
+        print(f"A palavra era {palavra}")
         break
